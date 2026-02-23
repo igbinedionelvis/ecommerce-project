7a790bc469a56a8a5ad83a1bb56b9d8aca9f8f1a
igbinedionelvis <eligbinosa@gmail.com>
2026-02-23 05:56:28 -0800

    Convert home and checkout to async await

--- src/pages/orders/OrdersPage.jsx
+++ src/pages/orders/OrdersPage.jsx
@@ -4,7 +4,8 @@ import { useState, useEffect, Fragment } from "react";
 import { Link } from "react-router";
 import { Header } from "../../components/Header";
 import { formatMoney } from "../../utils/money";
-import BuyAgainIcon from "../assets/images/icons/buy-again.png";
+import BuyAgainIcon from "../../assets/images/icons/buy-again.png";
+
 import "./OrdersPage.css";
 
 export function OrdersPage({ cart }) {
