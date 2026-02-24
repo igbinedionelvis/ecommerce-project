fbcdd0f970dcb30c3b0a9cba30aba4d45c63e57f
igbinedionelvis <eligbinosa@gmail.com>
2026-02-24 05:29:15 -0800

    Seperate orders summary to smaller components

---
+++ src/pages/checkout/DeliveryDate.jsx
@@ -0,0 +1,18 @@
+import dayjs from "dayjs";
+
+export function DeliveryDate({ cartItem, deliveryOptions }) {
+  const selectedDeliveryOption = deliveryOptions?.find(
+    (deliveryOption) => deliveryOption.id === cartItem?.deliveryOptionId,
+  );
+
+  return (
+    <div className="delivery-date">
+      Delivery date:{" "}
+      {selectedDeliveryOption
+        ? dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format(
+            "dddd, MMMM D",
+          )
+        : "Not selected"}
+    </div>
+  );
+}
