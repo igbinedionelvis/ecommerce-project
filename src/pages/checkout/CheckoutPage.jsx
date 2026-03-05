9a79dc5c34e9f06a2c961d1c487dff271ade3893
igbinedionelvis <eligbinosa@gmail.com>
2026-03-05 07:38:41 -0800

    Create update delivery option feature

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -2,45 +2,65 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { OrderSummary } from "./OrderSummary";
 import { PaymentSummary } from "./PaymentSummary";
-import { CheckoutHeader } from "./CheckoutHeader";
+import "./CheckoutHeader";
 import "./CheckoutPage.css";
 
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
-      try {
-        let response = await axios.get(
-          "/api/delivery-options?expand=estimatedDeliveryTime",
-        );
-        setDeliveryOptions(response.data);
+      let response = await axios.get(
+        "/api/delivery-options?expand=estimatedDeliveryTime",
+      );
+      setDeliveryOptions(response.data);
 
-        response = await axios.get("/api/payment-summary");
-        setPaymentSummary(response.data);
-      } catch (error) {
-        console.error("Checkout fetch error:", error);
-      }
+      response = await axios.get("/api/payment-summary");
+      setPaymentSummary(response.data);
     };
 
     fetchCheckoutData();
-  }, []);
+  }, [cart]);
 
   return (
     <>
       <title>Checkout</title>
-      <link rel="icon" type="image/svg+xml" href="cart-favicon.png " />
 
-      <CheckoutHeader cart={cart} />
+      <div className="checkout-header">
+        <div className="header-content">
+          <div className="checkout-header-left-section">
+            <a href="/">
+              <img className="logo" src="images/logo.png" />
+              <img className="mobile-logo" src="images/mobile-logo.png" />
+            </a>
+          </div>
+
+          <div className="checkout-header-middle-section">
+            Checkout (
+            <a className="return-to-home-link" href="/">
+              3 items
+            </a>
+            )
+          </div>
+
+          <div className="checkout-header-right-section">
+            <img src="images/icons/checkout-lock-icon.png" />
+          </div>
+        </div>
+      </div>
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+          <OrderSummary
+            cart={cart}
+            deliveryOptions={deliveryOptions}
+            loadCart={loadCart}
+          />
 
-          <PaymentSummary paymentSummary={paymentSummary} />
+          <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
         </div>
       </div>
     </>
